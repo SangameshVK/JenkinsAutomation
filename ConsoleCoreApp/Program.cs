97c4b1448f48d8f345c538da1fdc837e7e7d9977
SangameshVK <sangameshvishweshwar.kalagond@citrix.com>
2019-12-17 18:30:08 +0530

    Inital Hello World Commit

---
+++ ConsoleCoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First .NET Core with automated build");
+        }
+    }
+}
